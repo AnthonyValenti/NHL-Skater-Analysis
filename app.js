021de7d5695b883112b272de967c9ac23da4275f
Anthony Valenti <anthonyvalenti12@icloud.com>
2022-01-04 20:17:29 -0500

    Changed to MongoDB

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const ctx1 = document.getElementById('chart1').getContext('2d');
 const chart1 = new Chart(ctx1,{
     type: 'bar',
     data:{
-        labels: ['GF/60','GA/60'],
+        labels: ['GF/60','xGF/60','GA/60','xGA/60'],
         datasets: [
             {
-            label: 'Skater Charts',
+            label: 'Z Score',
             data: []   
             }
         ]
+    },
+    options: {
+        scales:{
+            y: {
+                min: -3,
+                max: 3
+
+            }
+        }
+
     }
 });
 
@@ -35,13 +45,23 @@ const ctx2 = document.getElementById('chart2').getContext('2d');
 const chart2 = new Chart(ctx2,{
     type: 'bar',
     data:{
-        labels: ['GF/60','GA/60'],
+        labels: ['GF/60','xGF/60','GA/60','xGA/60'],
         datasets: [
             {
-             label: 'Skater Charts',
+             label: 'Z Score',
              data: []   
             }
         ]
+    },
+    options: {
+        scales:{
+            y: {
+                min: -3,
+                max: 3
+
+            }
+        }
+
     }
 });
 
@@ -50,28 +70,70 @@ async function getData(name,chart){
     const response = await fetch('http://localhost:5501/api');
     const data = await response.json();
     for (item of data){
-        if(name==item.name){
+        if(name==item.Player){
             removeData(chart);
             removeData(chart);
-            addData(chart,'GF/60',item.gf);
-            addData(chart,'GA/60',item.ga);
-
+            removeData(chart);
+            removeData(chart);
+            addData(chart,'GF/60',((item.GF-data[0].GF)/data[1].GF));
+            addData(chart,'xGF/60',((item.xGF-data[0].xGF)/data[1].xGF));
+            addData(chart,'GA/60',((item.GA-data[0].GA)/data[1].GA));
+            addData(chart,'xGA/60',((item.xGA-data[0].xGA)/data[1].xGA));
 
         }
     }
 }
 
-function searchPlayer(){
+async function searchPlayer(){
+    var sugBox1 = document.getElementById('sugBox1');
+    var sugBox2 = document.getElementById('sugBox2');   
     var input1 = document.getElementById('search1');
+    const response = await fetch('http://localhost:5501/api');
+    const data = await response.json();
+    input1.addEventListener("input",function(event){
+        let matches = data.filter( player =>{
+        const regex = new RegExp(`^${input1.value}`,'gi');
+        return player.Player.match(regex)
+        }); 
+        if(matches.length>=0){
+            sugBox1.style.visibility="visible";
+            sugBox1.innerText = matches[0].Player;
+        }
+    });
+    sugBox1.onclick=function(){
+        input1.value=sugBox1.innerText;
+        getData(input1.value,chart1);
+        sugBox1.style.visibility="hidden";
+
+    };
+
     input1.addEventListener("keyup", function(event){
         if(event.key=='Enter'){
+            sugBox1.style.visibility="hidden";
             getData(input1.value,chart1);
         }
     });
 
     var input2 = document.getElementById('search2');
+    input2.addEventListener("input",function(event){
+        let matches2 = data.filter( player =>{
+        const regex2 = new RegExp(`^${input2.value}`,'gi');
+        return player.Player.match(regex2)
+        }); 
+        if(matches2.length>=0){
+            sugBox2.style.visibility="visible";
+            sugBox2.innerText = matches2[0].Player;
+        }
+    });
+    sugBox2.onclick=function(){
+        input2.value=sugBox2.innerText;
+        getData(input2.value,chart2);
+        sugBox2.style.visibility="hidden";
+
+    };
     input2.addEventListener("keyup", function(event){
         if(event.key=='Enter'){
+            sugBox2.style.visibility="hidden";
             getData(input2.value,chart2);            
         }
     });
--- server.js
+++ server.js
@@ -1,13 +1,44 @@
+//Imports and server setup
 const { response } = require('express');
+const { MongoClient } = require('mongodb');
+const url = "mongodb+srv://anthony:Pole13579@cluster1.bvl6p.mongodb.net/nhldata?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
 const express = require('express');
 const app = express();
 app.listen(5501, () => console.log('listening at 5501'));
 app.use('/',express.static('public'));
 app.use(express.json({ limit: '1mb' }));
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5502');
   next();
-});let query = 'SELECT DISTINCT Player name, Position pos, Team team, GF_60 gf, GA_60 ga FROM playerData';
+});
+getData();
+
+
+async function getData(){
+    try{
+        app.get('/api', async (req, res) => {
+            await client.connect();
+            const dataArr = [];
+            const collection = client.db("nhldata").collection("players");
+            collection.find().forEach(function(doc) {
+                dataArr.push(doc);
+            }, function(err) {
+                res.json(dataArr);
+            });
+        });
+    } catch(e){
+        console.log(e);
+    } finally{
+        await client.close();
+    }
+}
+
+
+
+/*
+let query = 'SELECT DISTINCT Player name, Position pos, Team team, GF_60 gf, GA_60 ga FROM playerData';
 const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('jsData.db',sqlite3.OPEN_READONLY, (err) => {
@@ -38,3 +69,4 @@ function closeDb() {
     console.log('Closed the DB');
     });
 }
+*/
